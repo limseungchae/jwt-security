ddf17cedfbb66616f700a6afe63b3190a58dede8
admin <tmdco12@naver.com>
2023-11-02 11:29:05 +0900

    Application - 설정 추가 엔티티 수정 시간 추적을 위한 감사 설정 추가

--- src/main/java/join/login/jwtsecurity/JwtSecurityApplication.java
+++ src/main/java/join/login/jwtsecurity/JwtSecurityApplication.java
@@ -2,7 +2,9 @@ package join.login.jwtsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // 엔티티 생성일 수정일 정보 자동설정, 엔티티 이력 추적
 @SpringBootApplication
 public class JwtSecurityApplication {
 
